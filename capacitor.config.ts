4e03909d0cd2b7bf99ae4228732d1b2066afbfce
Abin B N <abinjannath007@gmail.com>
2025-12-22 14:36:20 +0000

    check whether the capacitor is installed correctly

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.android.silence',
   appName: 'Silence',
-  webDir: 'public'
+  webDir: 'out'
 };
 
 export default config;
